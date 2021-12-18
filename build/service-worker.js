7c6e3bc8054e9f5643aa08a7a99c56374c845204
unknown <bnidheesh844@gmail.com>
2021-12-18 19:23:16 +0530

    latest build version

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vijay/abctestdfetest/precache-manifest.93eaeb9a8f6f25e7b22dddd71b5402e4.js"
+  "./precache-manifest.60e8b2b4d8f95761c4f266622cab2b26.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vijay/abctestdfetest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
