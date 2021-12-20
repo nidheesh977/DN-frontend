319b811950bc187170b99e7564c8414fd8abf967
unknown <bnidheesh844@gmail.com>
2021-12-20 14:41:09 +0530

    Latest build

---
+++ build/precache-manifest.4c4eda9a5c4b9ff6164b395b670ef97d.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01809cd2e6a64f5e27c9efd52cccf1b7",
+    "url": "/nidheesh/dn/DN-frontend/index.html"
+  },
+  {
+    "revision": "497043e95eeb98a26572",
+    "url": "/nidheesh/dn/DN-frontend/static/css/2.7bfc581e.chunk.css"
+  },
+  {
+    "revision": "8b0b11bd82ac0f17774b",
+    "url": "/nidheesh/dn/DN-frontend/static/css/main.ad352e53.chunk.css"
+  },
+  {
+    "revision": "497043e95eeb98a26572",
+    "url": "/nidheesh/dn/DN-frontend/static/js/2.e3be5a89.chunk.js"
+  },
+  {
+    "revision": "ddbce40c7fba85b02d328baa4231b87b",
+    "url": "/nidheesh/dn/DN-frontend/static/js/2.e3be5a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0b11bd82ac0f17774b",
+    "url": "/nidheesh/dn/DN-frontend/static/js/main.7b4be56f.chunk.js"
+  },
+  {
+    "revision": "531e0b395e8e8bee6f64",
+    "url": "/nidheesh/dn/DN-frontend/static/js/runtime-main.faeca219.js"
+  },
+  {
+    "revision": "e677355d3a12493485b6baf506c1cb23",
+    "url": "/nidheesh/dn/DN-frontend/static/media/Filter.e677355d.svg"
+  },
+  {
+    "revision": "f18060958e30ebad473c1e6043e7b372",
+    "url": "/nidheesh/dn/DN-frontend/static/media/Image.f1806095.svg"
+  },
+  {
+    "revision": "c2060efcee96464d06d13b11770fc8c5",
+    "url": "/nidheesh/dn/DN-frontend/static/media/Muli-Bold.c2060efc.ttf"
+  },
+  {
+    "revision": "da0d5fc284281a86c7c1416d630025a8",
+    "url": "/nidheesh/dn/DN-frontend/static/media/Muli-ExtraLight.da0d5fc2.ttf"
+  },
+  {
+    "revision": "9ea7b84129925c4fd62e3dfc9bae766b",
+    "url": "/nidheesh/dn/DN-frontend/static/media/Muli-Light.9ea7b841.ttf"
+  },
+  {
+    "revision": "877cf46a57ca3dbd6f2b864ec5621a3e",
+    "url": "/nidheesh/dn/DN-frontend/static/media/Muli-Regular.877cf46a.ttf"
+  },
+  {
+    "revision": "0c92e2300066a3c9e3ffc7c9850a0d1b",
+    "url": "/nidheesh/dn/DN-frontend/static/media/RotateImage.0c92e230.svg"
+  },
+  {
+    "revision": "80c884e61e67fba0175cad71afac58bd",
+    "url": "/nidheesh/dn/DN-frontend/static/media/RotateImage.80c884e6.svg"
+  },
+  {
+    "revision": "3aa09aff8fc7ab016bebd098e6822ed7",
+    "url": "/nidheesh/dn/DN-frontend/static/media/ThreeDImage.3aa09aff.svg"
+  },
+  {
+    "revision": "dc595ec71edcdd796fe412bb3ffd17cc",
+    "url": "/nidheesh/dn/DN-frontend/static/media/ThreeDImage.dc595ec7.svg"
+  },
+  {
+    "revision": "11e3f57bed36c66d7afeea76e4cc83ca",
+    "url": "/nidheesh/dn/DN-frontend/static/media/UploadFile.11e3f57b.svg"
+  },
+  {
+    "revision": "1d0151ef2de51c9cab01d7f7dc94b092",
+    "url": "/nidheesh/dn/DN-frontend/static/media/Video.1d0151ef.svg"
+  },
+  {
+    "revision": "9d4c347d99fe054f8cd376cbd93a2c2c",
+    "url": "/nidheesh/dn/DN-frontend/static/media/Video.9d4c347d.svg"
+  },
+  {
+    "revision": "38366f4910107037d1c8d824ae06d202",
+    "url": "/nidheesh/dn/DN-frontend/static/media/calendar.38366f49.svg"
+  },
+  {
+    "revision": "8a0944c110bea55f56b80d271d03b9b6",
+    "url": "/nidheesh/dn/DN-frontend/static/media/clock.8a0944c1.svg"
+  },
+  {
+    "revision": "eaa5387659431571464d5d2642c9a7b4",
+    "url": "/nidheesh/dn/DN-frontend/static/media/close.eaa53876.svg"
+  },
+  {
+    "revision": "fa37e9d556133902033f20b57c2509d9",
+    "url": "/nidheesh/dn/DN-frontend/static/media/cover-edit.fa37e9d5.svg"
+  },
+  {
+    "revision": "d6e8105f5e7174293c243d0c95e30872",
+    "url": "/nidheesh/dn/DN-frontend/static/media/cover-img.d6e8105f.svg"
+  },
+  {
+    "revision": "72c87b85913c0ab56cf2a36d490a2fb1",
+    "url": "/nidheesh/dn/DN-frontend/static/media/drone-img.72c87b85.svg"
+  },
+  {
+    "revision": "1fcd75f359e4c70563813d691716b26b",
+    "url": "/nidheesh/dn/DN-frontend/static/media/drone-person.1fcd75f3.svg"
+  },
+  {
+    "revision": "21fac4fd6076cea228114ab325dec123",
+    "url": "/nidheesh/dn/DN-frontend/static/media/dropdown-bottom.21fac4fd.svg"
+  },
+  {
+    "revision": "afc43f0d984017c9cee9bb9e20af34b7",
+    "url": "/nidheesh/dn/DN-frontend/static/media/dummy_5.afc43f0d.png"
+  },
+  {
+    "revision": "7f26a06d6f8ef6c1acbd90b64571491c",
+    "url": "/nidheesh/dn/DN-frontend/static/media/facebook.7f26a06d.svg"
+  },
+  {
+    "revision": "3969e606308448bacb0fc1b57fe86f73",
+    "url": "/nidheesh/dn/DN-frontend/static/media/google-pay.3969e606.svg"
+  },
+  {
+    "revision": "4acd14c827c852af4c8f545a6df3071b",
+    "url": "/nidheesh/dn/DN-frontend/static/media/hirebanner.4acd14c8.svg"
+  },
+  {
+    "revision": "51f42f32920e11f22188c758d4c9a747",
+    "url": "/nidheesh/dn/DN-frontend/static/media/hirebtn.51f42f32.svg"
+  },
+  {
+    "revision": "e3788ae0e3073322c722d55ebc8dacb1",
+    "url": "/nidheesh/dn/DN-frontend/static/media/hiring.e3788ae0.svg"
+  },
+  {
+    "revision": "b092ae553252b29d59855cc25cbc1198",
+    "url": "/nidheesh/dn/DN-frontend/static/media/image.b092ae55.svg"
+  },
+  {
+    "revision": "0bd01f34abc1f6c0ea106ae6b50f73e3",
+    "url": "/nidheesh/dn/DN-frontend/static/media/instagram.0bd01f34.svg"
+  },
+  {
+    "revision": "904fed2ebeb31532fb52fffc2fbb4af4",
+    "url": "/nidheesh/dn/DN-frontend/static/media/linkedin.904fed2e.svg"
+  },
+  {
+    "revision": "3aeb2060e78898817a83ee079651e9d5",
+    "url": "/nidheesh/dn/DN-frontend/static/media/loading.3aeb2060.svg"
+  },
+  {
+    "revision": "1b0a0df7b887bc993a12c9dd001a597a",
+    "url": "/nidheesh/dn/DN-frontend/static/media/location.1b0a0df7.svg"
+  },
+  {
+    "revision": "9d45b76c0ceecbf75ea3b51175d1f31d",
+    "url": "/nidheesh/dn/DN-frontend/static/media/noresultfound.9d45b76c.svg"
+  },
+  {
+    "revision": "9e773a4dfe237dd7775419078de96237",
+    "url": "/nidheesh/dn/DN-frontend/static/media/paytm.9e773a4d.svg"
+  },
+  {
+    "revision": "e1c467ec98133bde995355da11a3caa4",
+    "url": "/nidheesh/dn/DN-frontend/static/media/person.e1c467ec.svg"
+  },
+  {
+    "revision": "03940a18e179999089aec0328435832b",
+    "url": "/nidheesh/dn/DN-frontend/static/media/phonepe.03940a18.svg"
+  },
+  {
+    "revision": "c81c303be32514bac8d3faa3d5aaef14",
+    "url": "/nidheesh/dn/DN-frontend/static/media/pin.c81c303b.svg"
+  },
+  {
+    "revision": "e72bd502cbcc6f1c058c1e223a492e4f",
+    "url": "/nidheesh/dn/DN-frontend/static/media/pinterest.e72bd502.svg"
+  },
+  {
+    "revision": "0460a67f53c6ef5aa5ffff0b08720a0b",
+    "url": "/nidheesh/dn/DN-frontend/static/media/profile-edit.0460a67f.svg"
+  },
+  {
+    "revision": "133d6593b7474dde8e82b87f21649fea",
+    "url": "/nidheesh/dn/DN-frontend/static/media/profile.133d6593.svg"
+  },
+  {
+    "revision": "4eb6687d45f1c5389c85823b0f848364",
+    "url": "/nidheesh/dn/DN-frontend/static/media/purchase.4eb6687d.png"
+  },
+  {
+    "revision": "1056fa61ff122af85b9578683ae90cd3",
+    "url": "/nidheesh/dn/DN-frontend/static/media/twitter.1056fa61.svg"
+  },
+  {
+    "revision": "38dd4108b9ce47e0226e29898f88ebb7",
+    "url": "/nidheesh/dn/DN-frontend/static/media/upgradeproversion.38dd4108.svg"
+  },
+  {
+    "revision": "0e4f682568279964425f924bb0bbd8c7",
+    "url": "/nidheesh/dn/DN-frontend/static/media/upload.0e4f6825.svg"
+  },
+  {
+    "revision": "cf4689b898e3439c4be550f58c2480da",
+    "url": "/nidheesh/dn/DN-frontend/static/media/video-icon.cf4689b8.svg"
+  },
+  {
+    "revision": "b02488fb4dc725e35a6d6536b538fd7c",
+    "url": "/nidheesh/dn/DN-frontend/static/media/viewjob_mobile.b02488fb.svg"
+  },
+  {
+    "revision": "fc314f0b2baa7da24803bdbe4f4aced8",
+    "url": "/nidheesh/dn/DN-frontend/static/media/work.fc314f0b.svg"
+  },
+  {
+    "revision": "4e8764cff65b04880cd0738716bbc07a",
+    "url": "/nidheesh/dn/DN-frontend/static/media/youtube.4e8764cf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.60e8b2b4d8f95761c4f266622cab2b26.js"
+  "/nidheesh/dn/DN-frontend/precache-manifest.4c4eda9a5c4b9ff6164b395b670ef97d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nidheesh/dn/DN-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
