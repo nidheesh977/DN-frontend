b9725a992102622fe4076a36175cd54735d49062
Yaseen ahmed <yaseen.nexevo@gmail.com>
2022-03-18 17:34:15 +0530

    media work

--- src/components/website/VerifiedEmail.js
+++ src/components/website/VerifiedEmail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cover from "./company_dashboard/images/cover.jpg";
 import "./company_dashboard/css/Company_BasicInfo.css";
 import Pilot from "./company_dashboard/images/pilot.jpg";
@@ -8,34 +8,67 @@ import { Container, Row, Col, Hidden } from "react-grid-system";
 import Edit from "./company_dashboard/images/edit-1.svg";
 import PhoneInput from "react-phone-number-input";
 import All from "./All.module.css";
-import "../css/Common.css"
+import "../css/Common.css";
 import DronePerson from "../images/drone_person_new.png";
-import {useHistory} from "react-router-dom"
-
+import { useHistory, useParams } from "react-router-dom";
+import axios from "axios";
+const domain = process.env.REACT_APP_MY_API;
 
 function VerifiedEmail() {
-    const history = useHistory();
-
- 
+  let param = useParams();
+  let [message, setMessage] = useState("");
+  console.log(param.id);
+  useEffect(() => {
+    axios
+      .post(`${domain}/api/user/verifyMail/${param.id}/verify/${param.token}`)
+      .then((response) => {
+        console.log(response.data);
+        setMessage(response.data);
+      });
+  }, []);
+  const history = useHistory();
 
   return (
     <Container className={All.Container}>
- <Row>
-            <Col lg={7} className={All.DronePerson} >
-                <div >
-                    <img src={DronePerson} />
+      <Row>
+        <Col lg={7} className={All.DronePerson}>
+          <div>
+            <img src={DronePerson} />
+          </div>
+        </Col>
+        <Col lg={5}>
+          <div style={{ margin: "100px 0px" }}>
+            {message === "No token available" ? (
+              <h1>Oops something Went Wrong</h1>
+            ) : (
+              <div>
+                {" "}
+                <h2>Your Mail has been successfully Verified</h2>
+                <div>
+                  <p>Click below to complete your Profile or go to home Page</p>
+                  <div
+                    className="u_f_popup_btn1"
+                    style={{ display: "inline-block" }}
+                    onClick={() => {
+                      history.push("/");
+                    }}
+                  >
+                    Home Page
+                  </div>
+                  <div
+                    className="u_f_popup_btn2"
+                    style={{ display: "inline-block" }}
+                    onClick={() => {
+                      history.push("/choose-categories");
+                    }}
+                  >
+                    Complete Profile
+                  </div>
                 </div>
-            </Col>
-            <Col lg={5}>  
-            <div style={{margin:"100px 0px"}}>
-            <h2>Your Mail has been successfully Verified</h2>
-<div>
-    <p>Click below to complete your Profile or go to home Page</p>
-<div className="u_f_popup_btn1" style={{display:"inline-block"}} onClick={()=>{history.push("/")}}>Home Page</div> 
-<div className="u_f_popup_btn2"  style={{display:"inline-block"}} onClick={()=>{history.push("/choose-categories")}}>Complete Profile</div>
-</div>  
-</div>
-      </Col>
+              </div>
+            )}
+          </div>
+        </Col>
       </Row>
     </Container>
   );
--- src/components/website/pilot_dashboard/Pilot_approved3d.js
+++ src/components/website/pilot_dashboard/Pilot_approved3d.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-grid-system";
 import Picture from "./images/drone_img.jpg";
 import Premium from "./images/goldenStar.svg";
@@ -8,79 +8,98 @@ import downloadIcon from "../../images/downloadIcon.svg";
 import productLike from "../../images/product_like.png";
 import moreIcon from "../../images/Path.svg";
 import loadMore from "../../images/Group 71.svg";
+import All from '../../website/All.module.css'
+import axios from 'axios'
+const domain = process.env.REACT_APP_MY_API
 
 
 
-function mouseGotIN(id) {
-  document.getElementById("pd_likes/" + id).style.display = "block";
-  document.getElementById("pd_more/" + id).style.display = "block";
-  document.getElementById("pd_images_more/" + id).style.display = "none";
+
+function mouseGotIN(_id) {
+  document.getElementById("pd_likes/" + _id).style.display = "block";
+  document.getElementById("pd_more/" + _id).style.display = "block";
+  document.getElementById("pd_images_more/" + _id).style.display = "none";
 
 }
-function mouseGotOut(id) {
-  document.getElementById("pd_likes/" + id).style.display = "none";
-  document.getElementById("pd_more/" + id).style.display = "none";
+function mouseGotOut(_id) {
+  document.getElementById("pd_likes/" + _id).style.display = "none";
+  document.getElementById("pd_more/" + _id).style.display = "none";
 }
 
-function showMore(id){
-    document.getElementById("pd_images_more/" + id).style.display = "block";
+function showMore(_id){
+    document.getElementById("pd_images_more/" + _id).style.display = "block";
 }
 
 function Pilot_approved3d() {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("access_token"),
+    },
+  };
+  useEffect(() => {
+    axios.post(`${domain}/api/image/getApproved3d`,config).then(
+      (response) => {
+console.log(response.data)       
+  setValue(response.data)
+      }
+    );
+  }, []);
+  let [value, setValue] = useState([]);
+
   let details = {
     images: [
-      { id: 1, views: "5K", downloads: "2K", likes: "1K", premium: false, src: "https://wallpaperaccess.com/thumb/4896.jpg" },
-      { id: 2, views: "8K", downloads: "7K", likes: "4K",premium: true, src: 'https://wallpaperaccess.com/thumb/1667337.jpg'  },
-      { id: 3, views: "3K", downloads: "2K", likes: "1K", premium: false,  src: "https://wallpaperaccess.com/thumb/4896.jpg"  },
-      { id: 4, views: "9K", downloads: "3K", likes: "2K",premium: true, src: 'https://wallpaperaccess.com/thumb/1667337.jpg' },
-      { id: 5, views: "0K", downloads: "9K", likes: "3K",premium: false, src: "https://wallpaperaccess.com/thumb/370969.jpg" },
-      { id: 6, views: "5K", downloads: "8K", likes: "9K", premium: true,  src: "https://wallpaperaccess.com/thumb/4896.jpg"  },
-      { id: 7, views: "6K", downloads: "3K", likes: "6K",premium: false, src: "https://wallpaperaccess.com/thumb/370969.jpg" },
-      { id: 8, views: "7K", downloads: "6K", likes: "0K", premium: true, src: "https://wallpaperaccess.com/thumb/370969.jpg" },
-      { id: 9, views: "9K", downloads: "7K", likes: "8K", premium: true, src: 'https://wallpaperaccess.com/thumb/1667337.jpg'  },
+      { id: 1, views: "5K", downloads: "2K", likes: "1K", premium: false, src: "https://wallpaperaccess.com/thumb/324865.jpg" },
+      { id: 2, views: "8K", downloads: "7K", likes: "4K",premium: true, src: "https://wallpaperaccess.com/thumb/206585.jpg" },
+      { id: 3, views: "3K", downloads: "2K", likes: "1K", premium: false, src: "https://wallpaperaccess.com/thumb/324865.jpg" },
+      { id: 4, views: "9K", downloads: "3K", likes: "2K",premium: true, src: "https://wallpaperaccess.com/thumb/206585.jpg" },
+      { id: 5, views: "0K", downloads: "9K", likes: "3K",premium: false, src: "https://wallpaperaccess.com/thumb/87259.jpg" },
+      { id: 6, views: "5K", downloads: "8K", likes: "9K", premium: true, src: "https://wallpaperaccess.com/thumb/206585.jpg"  },
+      { id: 7, views: "6K", downloads: "3K", likes: "6K",premium: false , src: "https://wallpaperaccess.com/thumb/324865.jpg"},
+      { id: 8, views: "7K", downloads: "6K", likes: "0K", premium: true, src: "https://wallpaperaccess.com/thumb/87259.jpg" },
+      { id: 9, views: "9K", downloads: "7K", likes: "8K", premium: true, src: "https://wallpaperaccess.com/thumb/330474.jpg"  },
     ],
   };
 
   let [data, setData] = useState(details);
   return (
     <div>
-     <Row gutterWidth={12}>
-          {data.images.map((item) => {
+        <Row gutterWidth={12}>
+          {value.map((item) => {
             return (
               <Col  xl={4} lg={6} md={4} sm={6} xs={12}>
                 <div style={{ height: "310px" }}>
                   <div
                     className="pd_images_imageContainer"
-                    onMouseOver={() => mouseGotIN(item.id)}
-                    onMouseOut={() => mouseGotOut(item.id)}
+                    onMouseOver={() => mouseGotIN(item._id)}
+                    onMouseOut={() => mouseGotOut(item._id)}
                   >
-                    <img src={item.src} className="pd_images_image" />
+                    <img  src={`http://localhost:9000/${item.file}` }  className="pd_images_image" />
                     <div className={item.premium ? "pd_premiumBadge" : "pd_images_imageHidden"}>
                       <img src={premiumIcon} className="pd_premiumBadge_star" />
                     </div>
                   </div>
                   <div
                     className="pd_likes_container"
-                    id={"pd_likes/" + item.id}
+                    id={"pd_likes/" + item._id}
                   >
                     <img src={viewIcon} className="pd_likes_img" />{" "}
-                    <span>{item.views}</span>
+                    <span>{item.views.length}</span>
                     <img src={downloadIcon} className="pd_likes_img" />{" "}
-                    <span>{item.downloads}</span>
+                    <span>{item.downloads.length}</span>
                     <img src={productLike} className="pd_likes_img" />{" "}
-                    <span>{item.likes}</span>
+                    <span>{item.likes.length}</span>
                   </div>
                   <div
                     className="pd_moreBtn"
-                    id={"pd_more/" + item.id}
-                    onMouseOver={() => mouseGotIN(item.id)}
-                    onMouseOut={() => mouseGotOut(item.id)}
-                    onClick={()=>showMore(item.id)}
+                    id={"pd_more/" + item._id}
+                    onMouseOver={() => mouseGotIN(item._id)}
+                    onMouseOut={() => mouseGotOut(item._id)}
+                    onClick={()=>showMore(item._id)}
                   >
                      
                     <img src={moreIcon} className="pd_image_more" />
                   </div>
-                  <div className="pd_images_moreOptions" id={"pd_images_more/" + item.id}>
+                  <div className="pd_images_moreOptions" id={"pd_images_more/" + item._id}>
                       <div className="pd_images_moreOption">Edit</div>
                       <div className="pd_images_moreOption">Remove</div>
                       </div>
